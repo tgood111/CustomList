b2d4107245f1f9e6f041626358cc69fda337ae48
tgood111 <tgood111@live.com>
2017-08-17 18:22:55 -0500

    Created ToStringTests

---
+++ CustomListTest/ToStringTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class ToStringTest
+    {
+        [TestMethod]
+        public void IntToString()
+        {
+            CustomList<int> ints = new CustomList<int>();
+            ints.Add(2);
+            ints.Add(4);
+
+            string result = ints.ToString(); // "[2,4]"
+            string expected = "[2,4]";
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void CharListToString()
+        {
+            CustomList<char> chars = new CustomList<char>();
+            chars.Add('a');
+            chars.Add('b');
+            chars.Add('c');
+            chars.Add('d');
+
+            string result = chars.ToString(); // "[a,b,c,d]"
+            string expected = "[a,b,c,d]";
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
