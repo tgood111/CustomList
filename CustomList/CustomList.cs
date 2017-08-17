dccf12c9c8d91a1f61bff25ab251b281974e8771
tgood111 <tgood111@live.com>
2017-08-17 18:23:33 -0500

    Added unimplemented functions

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -3,10 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CustomList;
 
 namespace CustomList
 {
-    class CustomList
-    {
+    public class CustomList<T>
+    {   //private T[] array;
+        //public int Count {get; private set;}
+        //public static CustomList <T> operator +(CustomList<T> a, CustomList<T> b)
+        //{
+        //null;
+        //}
+        // The array in the background
+        private T[] array;
+
+        public int Count { get; private set; }
+
+        public static CustomList<T> operator +(CustomList<T> a, CustomList<T> b)
+        {
+            return null;
+        }
+
+        //Overloads [] operator, so that we can assign and reference values
+        public T this[int key]
+        {
+            // Reference
+            get
+            {
+                return GetValue(key);
+            }
+
+            // Assign
+            set
+            {
+                SetValue(key, value);
+            }
+        }
+
+        // Let's us add an element to the List
+        public void Add(T a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(CustomList<T> a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetValue(int key)
+        {
+            return array[key];
+        }
+
+        public void SetValue(int key, T value)
+        {
+            array[key] = value;
+        }
+
+        public void Zip(CustomList<T> b)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
