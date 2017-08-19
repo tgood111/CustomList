3f95354d69f41a1fcca4789fb65c238640088263
tgood111 <tgood111@live.com>
2017-08-19 10:44:04 -0500

    added addition operator tests

--- CustomListTest/operatorTest.cs
+++ CustomListTest/operatorTest.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
 
 namespace CustomListTest
 {
-    class operatorTest
+    [TestClass]
+    class OperatorTest
     {
+        [TestMethod]
+        public void TestAddittion()
+        {
+            CustomList<int> a = new CustomList<int>();
+            a.Add(1);
+            CustomList<int> b = new CustomList<int>();
+            b.Add(2);
+
+            CustomList<int> c = a + b;
+            Assert.AreEqual(1, c[0]);
+            Assert.AreEqual(2, c[1]);
+        }
     }
 }
